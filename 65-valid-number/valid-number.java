9cb08f7b300bab79fc819d42539d1ea31d2bd57f
Punith Puligorla <punithpuligorla.usa@gmail.com>
2025-08-20 11:59:44 -0400

    Time: 2 ms (30.85%) | Memory: 42.5 MB (87.50%) - LeetSync

---
+++ 65-valid-number/valid-number.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isNumber(String s) {
+     s = s.trim();
+
+        boolean pointSeen = false;
+        boolean eSeen = false;
+        boolean numberSeen = false;
+        boolean numberAfterE = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                numberSeen = true;
+                numberAfterE = true;
+            } else if (c == '.') {
+                if (eSeen || pointSeen) {
+                    return false;
+                }
+                pointSeen = true;
+            } else if (c == 'e' || c == 'E') {
+                if (eSeen || !numberSeen) {
+                    return false;
+                }
+                eSeen = true;
+                numberAfterE = false;
+            } else if (c == '+' || c == '-') {
+                if (i > 0 && s.charAt(i-1) != 'e' && s.charAt(i-1) != 'E') {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        return numberSeen && numberAfterE;   
+    }
+}
