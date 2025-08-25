cb7a0c1c821203e5ce93faa9249fd8c888053d8a
Punith Puligorla <punithpuligorla.usa@gmail.com>
2025-08-25 12:23:43 -0400

    Time: 0 ms (100.00%) | Memory: 40.8 MB (29.14%) - LeetSync

---
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numTilings(int n) {
+     if (n == 1) {
+            return 1;
+        }
+        long MOD = 1_000_000_007;
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        if (n >= 2) {
+            dp[2] = 2;
+        }
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MOD;
+        }
+        return (int) dp[n];
+            
+    }
+}
